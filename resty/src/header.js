4b288dcfbb48b4190837509fd9d8ba74c3259da7
Arthur Lozano <chillout885@gmail.com>
2021-05-13 23:45:08 -0400

    Resty

---
+++ resty/src/header.js
@@ -0,0 +1,11 @@
+// import React, { Component, Fragment } from 'react';
+import './header.scss';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>New header</h1>
+    </header>
+  );
+};
+export default Header;
